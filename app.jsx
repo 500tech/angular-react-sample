9b7f43718cd8b1d51ee7c21958a400231a3045e6
Boris Dinkevich <boris@dinkevich.com>
2015-05-04 23:07:25 -0400

    Speed demo

--- app.jsx
+++ app.jsx
@@ -6,11 +6,49 @@ var React = require('react/addons');
 
 angular.module("sampleapp", []);
 
+window.array = [];
+
+for (var i = 0; i < 100; i++) {
+  window.array.push(i);
+}
+
+window.lastUpdate = Date.now();
+window.delay = 0;
+
+function updateNumber(i) {
+  window.delay = Date.now() - window.lastUpdate;
+  window.lastUpdate = Date.now();
+
+  i += 1;
+  if (i > 20) 
+    i = 0;
+  return i
+}
+
+function SpeedController($interval) {
+  var ctrl = this;
+  ctrl.delay = window.delay;
+
+  this.getNumber = function() { return window.array };
+
+  this.number = 10;
+
+  function tick() {
+    ctrl.number = updateNumber(ctrl.number);
+    ctrl.delay = window.delay;
+  }
+
+  $interval(tick, 1);
+}
+
+
 angular.module("sampleapp")
   .directive("sampleDirective", function() {
     return {
       restrict: 'E',
-      template: "<h2>Angular directive</h2>"
+      controller: SpeedController,
+      controllerAs: 'speed',
+      templateUrl: "template.html"
     }
   });
   
@@ -27,9 +65,45 @@ angular.module("sampleapp")
 
 
 var MyComponent = React.createClass({
+  getInitialState: function() {
+    return { number: 10, delay: window.delay };
+  },
+
+  componentDidMount: function() {
+    function tick() {
+      this.setState({ number: updateNumber(this.state.number), delay: window.delay });
+    }
+    setInterval(tick.bind(this), 1);
+  },
+
   render: function() {
+    var renderColumns = function(i) {
+      var style = {
+        'width': this.state.number + 'px',
+        'height': this.state.number + 'px'
+      }
+      return (
+        <td key={ i }>
+          <div className="cell" style={ style }></div>
+        </td>
+      );
+    }
+
+    var renderRows = function(i) {
+      return (
+        <tr key={ i }>
+          { window.array.map(renderColumns.bind(this)) }
+        </tr>
+      );
+    }
+
     return (
-      <h2>React Component</h2>
+      <div>
+        <h2>React Component <span className='delay'>{ this.state.delay } ms</span></h2>
+        <table>
+          { window.array.map(renderRows.bind(this)) }
+        </table>
+      </div>
     );
   }
 });
